3d35a5b198154c7a7456b97eb2fd868449d2d9a7
Budosama <achesilaoagus@gmail.com>
2025-01-23 20:42:39 -0300

    registrar cuenta link iol

--- obj/Debug/net8.0/InvertirOnlineApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/InvertirOnlineApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvertirOnlineApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f59e23a438d6e93049f5aab7e316483ec93872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32fadb0ab64b59b5a759f6e4223b241f743716d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvertirOnlineApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvertirOnlineApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
